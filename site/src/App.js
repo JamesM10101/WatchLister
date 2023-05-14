68ed7d48160d1a2bc80f8f018232b1f4a9673144
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-05-14 01:01:11 -0400

    Added Routes and Pages

--- site/src/App.js
+++ site/src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from "./pages/HomePage.jsx"
+import StartPage from "./pages/StartPage.jsx"
+import MoviePage from "./pages/MoviePage.jsx"
+import ReviewPage from "./pages/ReviewPage.jsx"
+import ProfilePage from "./pages/ProfilePage.jsx"
+
 function App() {
-  return <div></div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/movie/:movieId" element={<MoviePage />} />
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+        <Route path="/review/:reviewId" element={<ReviewPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
