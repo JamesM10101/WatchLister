7ac4b4d6f814ad73a71dbb157e3dc880948c834f
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-05-20 20:03:30 -0400

    mui theme setup

--- site/src/App.js
+++ site/src/App.js
@@ -1,4 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useMemo } from "react"
+import { useSelector } from "react-redux"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { createTheme } from "@mui/material/styles"
+import { themeSettings } from "./theme"
+
 import HomePage from "./pages/HomePage.jsx"
 import StartPage from "./pages/StartPage.jsx"
 import MoviePage from "./pages/MoviePage.jsx"
@@ -6,15 +12,21 @@ import ReviewPage from "./pages/ReviewPage.jsx"
 import ProfilePage from "./pages/ProfilePage.jsx"
 
 function App() {
+  const mode = useSelector((state) => state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<StartPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/movie/:movieId" element={<MoviePage />} />
-        <Route path="/profile/:userId" element={<ProfilePage />} />
-        <Route path="/review/:reviewId" element={<ReviewPage />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/movie/:movieId" element={<MoviePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+          <Route path="/review/:reviewId" element={<ReviewPage />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   )
 }
