d9abe1999046164c2ff1760d23acfa640d6e1fba
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-05-28 01:19:10 -0400

    Added ability to delete review

--- site/src/components/shared/MovieReviewCard.jsx
+++ site/src/components/shared/MovieReviewCard.jsx
@@ -1,6 +1,7 @@
 import { useTheme } from "@emotion/react"
 import {
   Circle,
+  Delete,
   Edit,
   Star,
   ThumbDownAlt,
@@ -8,7 +9,7 @@ import {
   ThumbUpAlt,
   ThumbUpAltOutlined,
 } from "@mui/icons-material"
-import { Box, Card, IconButton, Typography } from "@mui/material"
+import { Alert, Box, Card, IconButton, Typography } from "@mui/material"
 import React, { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import FlexBetween from "./FlexBetween"
@@ -16,6 +17,8 @@ import CreateReviewCard from "../shared/CreateReviewCard.jsx"
 
 function MovieReviewCard({ movie, reviewId, token }) {
   const [review, setReview] = useState({})
+  const [severity, setSeverity] = useState("")
+  const [alertMsg, setAlertMsg] = useState("")
   const [isLiked, setIsLiked] = useState(false)
   const [isDisliked, setIsDisliked] = useState(false)
   const [showFullDesc, setShowFullDesc] = useState(false)
@@ -31,11 +34,13 @@ function MovieReviewCard({ movie, reviewId, token }) {
         "Content-Type": "application/json",
       },
     }).then(async (res) => {
-      const result = await res.json()
-      setReview(result)
-      if (user) {
-        setIsLiked(result.likes[user._id])
-        setIsDisliked(result.dislikes[user._id])
+      if (res.status == 200) {
+        const result = await res.json()
+        setReview(result)
+        if (user && result) {
+          setIsLiked(result.likes[user._id])
+          setIsDisliked(result.dislikes[user._id])
+        }
       }
     })
   }
@@ -62,6 +67,32 @@ function MovieReviewCard({ movie, reviewId, token }) {
     })
   }
 
+  const deleteReview = async () => {
+    await fetch(`http://localhost:3001/reviews/${reviewId}/delete`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+        userId: user._id,
+      },
+    }).then(async (res) => {
+      if (res.status == 200) {
+        setSeverity("success")
+        setAlertMsg("Review Deleted")
+      } else {
+        setSeverity("error")
+        setAlertMsg("Could Not Delete")
+      }
+    })
+  }
+
+  const alertDeletion = () => {
+    const shouldDelete = window.confirm(
+      "Are you sure you would like to delete this post?"
+    )
+    if (shouldDelete) deleteReview()
+  }
+
   return isReviewEdit ? (
     <CreateReviewCard
       movieId={movie._id}
@@ -73,7 +104,7 @@ function MovieReviewCard({ movie, reviewId, token }) {
       setIsReviewEdit={setIsReviewEdit}
       setReviewForMovie={setReview}
     />
-  ) : (
+  ) : review ? (
     <Card
       width="100%"
       sx={{
@@ -100,14 +131,25 @@ function MovieReviewCard({ movie, reviewId, token }) {
           </Typography>
         </Typography>
 
-        {/* Edit Button */}
-        {user
-          ? review.userId === user._id && (
-              <IconButton onClick={() => setIsReviewEdit(true)}>
-                <Edit />
-              </IconButton>
-            )
-          : ""}
+        <Box>
+          {/* Delete Button */}
+          {user
+            ? review.userId === user._id && (
+                <IconButton onClick={alertDeletion}>
+                  <Delete />
+                </IconButton>
+              )
+            : ""}
+
+          {/* Edit Button */}
+          {user
+            ? review.userId === user._id && (
+                <IconButton onClick={() => setIsReviewEdit(true)}>
+                  <Edit />
+                </IconButton>
+              )
+            : ""}
+        </Box>
       </FlexBetween>
 
       {/* Review Title */}
@@ -217,7 +259,15 @@ function MovieReviewCard({ movie, reviewId, token }) {
             : 0}
         </Typography>
       </Box>
+
+      {severity && (
+        <Alert severity={severity}>
+          <Typography>{alertMsg}</Typography>
+        </Alert>
+      )}
     </Card>
+  ) : (
+    <></>
   )
 }
 
