42ef5131244ce4db1492ddaf2391f8806084b69c
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-05-28 17:24:51 -0400

    Homepage Movie Loading Quick Fix

--- site/src/pages/HomePage.jsx
+++ site/src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
-import { useSelector } from "react-redux"
-import MovieCard from "../components/shared/MovieCard"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { setMovies } from "../state/state"
+import { Box } from "@mui/material"
+import MovieCarousel from "../components/shared/MovieCarousel"
 
-function HomePage() {
+const HomePage = () => {
+  const dispatch = useDispatch()
+  const movies = useSelector((state) => state.movies)
   const token = useSelector((state) => state.token)
 
   // get 10 movies
@@ -22,23 +27,29 @@ function HomePage() {
     let movies = []
 
     for (let id of movieIds) {
-      const movie = await fetch(`http://localhost:5001/movies/${id}`, {
+      await fetch(`http://localhost:3001/movies/${id}`, {
         method: "GET",
         headers: {
           Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
-      }).then((res) => movies.push(res.body))
+      }).then(async (res) => movies.push(await res.json()))
     }
 
-    console.log(movies)
-
-    return movies
+    dispatch(setMovies({ movies: movies }))
   }
 
-  return getMovies().array.forEach((movie) => (
-    <MovieCard movie={movie}></MovieCard>
-  ))
+  useEffect(() => {
+    getMovies()
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <Box sx={{ width: "80%", margin: "auto" }}>
+      <Box marginTop={"1rem"} display="flex" flexDirection="column">
+        <MovieCarousel movies={movies} />
+      </Box>
+    </Box>
+  )
 }
 
 export default HomePage
