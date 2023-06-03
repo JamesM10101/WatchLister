a32724578c1fda8114c51c37a73962138e608c00
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-06-03 17:47:18 -0400

    MovieCarousel now displays title

--- site/src/components/shared/MovieCarousel.jsx
+++ site/src/components/shared/MovieCarousel.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react"
-import { Box, IconButton, useMediaQuery, useTheme } from "@mui/material"
+import {
+  Box,
+  IconButton,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material"
 import { ArrowBack, ArrowForward } from "@mui/icons-material"
 import MovieCard from "./MovieCard"
 
-function MovieCarousel({ movies }) {
+function MovieCarousel({ movies, title }) {
   const palette = useTheme().palette
 
   // keep track of starting index, needed for screen resizing
@@ -14,57 +20,69 @@ function MovieCarousel({ movies }) {
   const isFullSizeScreen = useMediaQuery("(min-width: 700px)")
 
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      gap="1rem"
-      sx={{
-        scale: isFullSizeScreen ? "1" : "0.8",
-      }}
-    >
-      {/* Back Button */}
-      <IconButton
-        disabled={(isFullSizeScreen ? startIndex : startIndexMobile) <= 0}
+    <div>
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <Typography variant="h3" fontWeight="bold">
+          {title}
+        </Typography>
+      </div>
+      <Box
+        marginTop={isFullSizeScreen ? ".5rem" : "-.75rem"}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        gap="1rem"
         sx={{
-          backgroundColor: palette.background.alt,
-        }}
-        onClick={() => {
-          setStartIndex(startIndex - 5)
-          setEndIndex(endIndex - 5)
-          setStartIndexMobile(startIndexMobile - 3)
-          setEndIndexMobile(endIndexMobile - 3)
+          scale: isFullSizeScreen ? "1" : "0.8",
         }}
       >
-        <ArrowBack />
-      </IconButton>
+        {/* Back Button */}
+        <IconButton
+          disabled={(isFullSizeScreen ? startIndex : startIndexMobile) <= 0}
+          sx={{
+            backgroundColor: palette.background.alt,
+          }}
+          onClick={() => {
+            setStartIndex(startIndex - 5)
+            setEndIndex(endIndex - 5)
+            setStartIndexMobile(startIndexMobile - 3)
+            setEndIndexMobile(endIndexMobile - 3)
+          }}
+        >
+          <ArrowBack />
+        </IconButton>
 
-      {/* Movies */}
-      {movies
-        .slice(
-          isFullSizeScreen ? startIndex : startIndexMobile,
-          isFullSizeScreen ? endIndex : endIndexMobile
-        )
-        .map((movie) => (
-          <MovieCard key={movie._id} movie={movie} />
-        ))}
+        {/* Movies */}
+        {movies
+          .slice(
+            isFullSizeScreen ? startIndex : startIndexMobile,
+            isFullSizeScreen ? endIndex : endIndexMobile
+          )
+          .map((movie) => (
+            <MovieCard key={movie._id} movie={movie} />
+          ))}
 
-      {/* Forward Button */}
-      <IconButton
-        disabled={
-          (isFullSizeScreen ? endIndex : endIndexMobile) >= movies.length
-        }
-        sx={{ backgroundColor: palette.background.alt }}
-        onClick={() => {
-          setStartIndex(startIndex + 5)
-          setEndIndex(endIndex + 5)
-          setStartIndexMobile(startIndexMobile + 3)
-          setEndIndexMobile(endIndexMobile + 3)
-        }}
-      >
-        <ArrowForward />
-      </IconButton>
-    </Box>
+        {/* Forward Button */}
+        <IconButton
+          disabled={
+            (isFullSizeScreen ? endIndex : endIndexMobile) >= movies.length
+          }
+          sx={{ backgroundColor: palette.background.alt }}
+          onClick={() => {
+            setStartIndex(startIndex + 5)
+            setEndIndex(endIndex + 5)
+            setStartIndexMobile(startIndexMobile + 3)
+            setEndIndexMobile(endIndexMobile + 3)
+          }}
+        >
+          <ArrowForward />
+        </IconButton>
+      </Box>
+    </div>
   )
 }
 
