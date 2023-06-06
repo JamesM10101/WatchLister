eca1c96f246e3f753a296cd5b9dfb0394972f928
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2023-06-05 23:34:30 -0400

    Timestamps added to User and Review models

--- server/models/Review.js
+++ server/models/Review.js
@@ -1,34 +1,37 @@
 import mongoose from "mongoose"
 
-const ReviewSchema = new mongoose.Schema({
-  movieId: {
-    type: String,
-    required: true,
+const ReviewSchema = new mongoose.Schema(
+  {
+    movieId: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    likes: {
+      type: Map,
+      of: Boolean, // userId : true
+    },
+    dislikes: {
+      type: Map,
+      of: Boolean, // userId : true
+    },
   },
-  userId: {
-    type: String,
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  rating: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-  },
-  likes: {
-    type: Map,
-    of: Boolean, // userId : true
-  },
-  dislikes: {
-    type: Map,
-    of: Boolean, // userId : true
-  },
-})
+  { timestamps: true }
+)
 
 const Review = mongoose.model("Review", ReviewSchema)
 export default Review
--- server/models/User.js
+++ server/models/User.js
@@ -1,45 +1,48 @@
 import mongoose from "mongoose"
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    min: 2,
-    max: 30,
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    savedMovies: {
+      type: Array,
+      of: String, // movieId
+    },
+    reviews: {
+      type: Array,
+      of: String, // reviewId
+    },
+    likes: {
+      type: Map,
+      of: Boolean, // reviewId : true
+    },
+    admin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    max: 50,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 5,
-  },
-  picturePath: {
-    type: String,
-    default: "",
-  },
-  savedMovies: {
-    type: Array,
-    of: String, // movieId
-  },
-  reviews: {
-    type: Array,
-    of: String, // reviewId
-  },
-  likes: {
-    type: Map,
-    of: Boolean, // reviewId : true
-  },
-  admin: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-})
+  { timestamps: true }
+)
 
 const User = mongoose.model("User", UserSchema)
 export default User
